678bdeefa797e76d289ec728a2c2f2e7ece49d3c
zhang_ying <zhang_ying@suixingpay.com>
2019-08-04 21:23:15 +0800

    添加gsonformat插件生成代码

--- Module01/src/com/larry/bean/Student3.java
+++ Module01/src/com/larry/bean/Student3.java
@@ -1,5 +1,7 @@
 package com.larry.bean;
 
+import java.util.List;
+
 /**
  * @version V1.0
  * @Author: Larry(PC)
@@ -8,4 +10,106 @@ package com.larry.bean;
  * 创建日期：2019/7/17 19:14
  */
 public class Student3 extends Person{
+
+    /**
+     * animals : {"dog":[{"name":"Rufus","breed":"labrador","count":1,"twoFeet":false},{"name":"Marty","breed":"whippet","count":1,"twoFeet":false}],"cat":{"name":"Matilda"}}
+     */
+
+    private AnimalsBean animals;
+
+    public AnimalsBean getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(AnimalsBean animals) {
+        this.animals = animals;
+    }
+
+    public static class AnimalsBean {
+        /**
+         * dog : [{"name":"Rufus","breed":"labrador","count":1,"twoFeet":false},{"name":"Marty","breed":"whippet","count":1,"twoFeet":false}]
+         * cat : {"name":"Matilda"}
+         */
+
+        private CatBean cat;
+        private List<DogBean> dog;
+
+        public CatBean getCat() {
+            return cat;
+        }
+
+        public void setCat(CatBean cat) {
+            this.cat = cat;
+        }
+
+        public List<DogBean> getDog() {
+            return dog;
+        }
+
+        public void setDog(List<DogBean> dog) {
+            this.dog = dog;
+        }
+
+        public static class CatBean {
+            /**
+             * name : Matilda
+             */
+
+            private String name;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+
+        public static class DogBean {
+            /**
+             * name : Rufus
+             * breed : labrador
+             * count : 1
+             * twoFeet : false
+             */
+
+            private String name;
+            private String breed;
+            private int count;
+            private boolean twoFeet;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public String getBreed() {
+                return breed;
+            }
+
+            public void setBreed(String breed) {
+                this.breed = breed;
+            }
+
+            public int getCount() {
+                return count;
+            }
+
+            public void setCount(int count) {
+                this.count = count;
+            }
+
+            public boolean isTwoFeet() {
+                return twoFeet;
+            }
+
+            public void setTwoFeet(boolean twoFeet) {
+                this.twoFeet = twoFeet;
+            }
+        }
+    }
 }
